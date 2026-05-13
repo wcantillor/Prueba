f1733969267132a8b192d054587c20bd06091d9b
Wilson Cantillo <50993659+wcantillor@users.noreply.github.com>
2026-05-12 20:43:34 -0500

    Create Calculadora.java
    Esta versión solo multipica dos número hardcoded.

---
+++ Calculadora.java
@@ -0,0 +1,24 @@
+// Tercera clase JAVA
+package com.mycompany.calculadora;
+    /* Así se crean
+    Comentarios de varias 
+    lineas
+     */
+
+    /** 
+     * Crear una documentación profesional de nuestro código.
+     * @author : Wilson Cantillo
+     * @version : 1.0
+      */
+public class Calculadora {
+
+    public static void main ( String [] args ) {
+
+    System .out . println (" --- Ejecutando -- Calculadora ---");
+
+    double n1 = 10.5;
+    double n2 = 5.2;
+
+    System .out . println (" Resultado :" + (n1 + n2));
+    }
+}
